f03f90caa71b461b65b3f1fed14543299146a034
Seth Frutiger <sethfrutiger@gmail.com>
2022-01-29 12:18:40 -0500

    initial commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+
+
+const Homepage = () => {
+    return (
+      <div>
+        <NavBar />
+        <h1>Hello from Homepage</h1>
+      </div>
+    );
+  };
+  
+  export default Homepage;
+  
---
+++ src/components/Shop.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+const Shop = () => {
+    return (
+      <div>
+        <NavBar />
+        <h1>Hello from shop</h1>
+      </div>
+    );
+  };
+  
+  export default Shop;
